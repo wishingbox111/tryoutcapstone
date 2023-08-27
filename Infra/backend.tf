af62ed4c6901fceececf66ab4b72047e5dda594f
wishingbox111 <wishingbox111@gmail.com>
2023-08-27 18:55:10 +0800

    add terraform workflow

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "group1-terrastate-bucket_chen"
+    bucket = "group1-terrastate-bucket-chen"
     key    = "group1-terrastate-s3.tfstate"
     region = "ap-southeast-1"
   }
